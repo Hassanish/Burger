20fff09c1213d2671d70e4ac5e923d404cd61427
burger <36898041+Hassanish@users.noreply.github.com>
2018-04-16 20:13:45 -0500

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,26 @@
+var orm = require('../config/orm.js');
 
+var table = "burgers";
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll(table, function (res) {
+      cb(res);
+    });
+  },
+ 
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne(table, cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne(table, objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition, cb) {
+    orm.delete(table, condition, function (res) {
+      cb(res);
+    });
+  }
+};
